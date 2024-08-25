2a06e5a32bf2183281f25eae697693e116f1f45b
KOSASIH <kosasihg88@gmail.com>
2024-08-25 11:28:35 +0700

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,24 @@
+# Constants file
+
+# API Endpoints
+API_ENDPOINT_IPFS = 'https://ipfs.infura.io:5001'
+API_ENDPOINT_ETHEREUM = 'https://mainnet.infura.io/v3/YOUR_PROJECT_ID'
+
+# Ethereum Network IDs
+MAINNET_NETWORK_ID = 1
+ROPSTEN_NETWORK_ID = 3
+RINKEBY_NETWORK_ID = 4
+GANACHE_NETWORK_ID = 1337
+
+# IPFS Pinning Services
+PINATA_PINNING_SERVICE = 'https://api.pinata.cloud/pinning/pinFileToIPFS'
+INFURA_PINNING_SERVICE = 'https://ipfs.infura.io:5001/api/v0/pin/add'
+
+# Ethereum Gas Prices (in GWei)
+GAS_PRICE_FAST = 20
+GAS_PRICE_NORMAL = 10
+GAS_PRICE_SLOW = 5
+
+# Other Constants
+MAX_RETRIES = 5
+TIMEOUT = 30
